cb672e473655fb7bcecaba1759d2b5800a202c40
cedrick J Randriamilamina <cedrickrandria@gmail.com>
2022-04-04 08:59:05 -0400

    modify route create anotation

--- annotatedappapi-master/routes/annotate.py
+++ annotatedappapi-master/routes/annotate.py
@@ -1,8 +1,16 @@
-import datetime
-from fastapi import APIRouter, Query, Response
+from turtle import home
+from fastapi import APIRouter,Depends
 from services.annotate_query_services import AnnotateQueryService
 from models.annotate import Annotate
 from models.annoate_test import AnnotateTest
+from models.annotations import Annotations
+from schemas.annotations import AnnotationsCreate
+from sqlalchemy.orm import Session
+from typing import Any
+from api import  deps
+from crud import crudAnnotation
+# import the error handling libraries for psycopg2
+from psycopg2 import OperationalError
 
 router = APIRouter()
 service = AnnotateQueryService()
@@ -18,3 +26,24 @@ async def insert_home_annoate(annotate:AnnotateTest):
 @router.put('/home')
 async def update_home_annotate(home:str, start:int, end:int, annotate:AnnotateTest):
     return service.update_home_annotate(home, start, end, annotate)
+
+@router.post("/createAnnotations", response_model=AnnotationsCreate)
+def create_annotation(
+    *,
+    db: Session = Depends(deps.get_db),
+    annotation_in:AnnotationsCreate
+) -> Any:
+    """
+    Create new Annotation.
+    """
+    print(annotation_in)
+    try:
+        annotation = crudAnnotation.annotations.create(db, obj_in=annotation_in)
+        return annotation
+    except OperationalError as error:
+        print ("Oops! An exception has occured:", error)
+        print ("Exception TYPE:", type(error))
+        return {
+            "code":"505",
+            "message":"Oops! An exception has occured:"
+        }
