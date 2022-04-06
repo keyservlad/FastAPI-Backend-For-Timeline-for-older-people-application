25bf45c292286fb1fdbbe2450ebc68e9cf731e8a
cedrick J Randriamilamina <cedrickrandria@gmail.com>
2022-04-06 19:08:21 -0400

    modify route create anotation

--- annotatedappapi-master/models/annotations.py
+++ annotatedappapi-master/models/annotations.py
@@ -1,5 +1,4 @@
 from sqlalchemy import Boolean, Column, Integer, String,DateTime
-from sqlalchemy.orm import relationship
 
 from db.base_class import Base
 
@@ -7,9 +6,9 @@ from db.base_class import Base
 
 class Annotations(Base):
     id = Column(Integer, primary_key=True, index=True)
-    home:  Column(String, index=False)
-    room:  Column(String, index=False)
-    start: Column(DateTime, index=False)
-    end: Column(DateTime, index=False)
-    activity_type: Column(String, index=False)
-    status: Column(String, index=False)
+    home=  Column(String, index=False)
+    room = Column(String, index=False)
+    start= Column(DateTime, index=False)
+    end=Column(DateTime, index=False)
+    activity_type= Column(String, index=False)
+    status= Column(String, index=False)
