0ccdbac0cfa47d9f057b01d6c70438fc4e3a739b
Rodrigues <jason.rodrigues@edu.esiee.fr>
2022-04-20 13:36:50 -0400

    modified endpoints following Front End requirements

--- annotatedappapi-master/routes/annotateController.py
+++ annotatedappapi-master/routes/annotateController.py
@@ -5,36 +5,55 @@ from annotate import Annotate
 from annotate_query_services import AnnotateQueryService
 from Activity import Activity
 
-
 router = APIRouter()
 service = AnnotateQueryService()
 
-@router.get('/annotations/id/{id}')
-def getAnnotation(id: int):
+
+
+# Obtenir la liste des annotations
+@router.get('/annotations')
+def getAnnotations():
     try:
         return null
     except IndexError:
-        raise HTTPException(status_code=404, detail="No annotation existing with this id")
+        raise HTTPException(status_code=404, detail="No annotation existing ")
 
 
+# Ajouter une annotation
 @router.post('/addAnnotation', status_code=201)
 def addAnnotation(annotation: Annotate):
     return null
 
 
+# Modifier une annotation
+@router.put('/modifyAnnotation/id/{id}')
+def modifyAnnotation(id: int):
+    return null
+
+
+# Delete une annotation
+@router.delete('/deleteAnnotation/id/{id}')
+def deleteAnnotation(id: int):
+    return null
+
+
+# Ajouter un type d'activité
 @router.post('/addActivity', status_code=201)
 def addActivity(activity: Activity):
     return null
 
 
-@router.get('models/Activities.py/id/{id}')
-def getActivity(id: int):
+
+# Obtenir la liste des activités possibles
+@router.get('/activities')
+def getActivities():
     try:
         return null
     except IndexError:
-        raise HTTPException(status_code=404, detail="No activity existing with this id")
+        raise HTTPException(status_code=404, detail="No activity existing")
 
 
-@router.delete('models/Activities.py/id/{id}')
+# Delete un type d'activité
+@router.delete('/deleteActivity/id/{id}')
 def deleteActivity(id: int):
     return null
